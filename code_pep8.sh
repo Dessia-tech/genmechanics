f7dc28c7ae691bbf36f1ad54bb97f776e9ea4253
Steven Masfaraud <masfaraud@dessia.tech>
2022-01-10 16:38:43 +0100

    ci: fix

--- code_pep8.sh
+++ code_pep8.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # check pep8 formatting for all files
 
-PEP8_CMD_TO_RUN='python3 -m autopep8 -i tests volmdlr/*.py'
+PEP8_CMD_TO_RUN='python3 -m autopep8 -i tests genmechanics/*.py'
 
-DETECTED_CHANGES=$(python3 -m autopep8 -d volmdlr/*.py)
+DETECTED_CHANGES=$(python3 -m autopep8 -d genmechanics/*.py)
 if [[ -n "$DETECTED_CHANGES" ]]
   then
   echo -e "\npep8 non conforming changes detected, please run :\n"
